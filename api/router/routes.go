663ff035c620c19f5df4521868aa06a4ecc4f2e1
Felipe Fernandes Versiane <felipeversiane09@gmail.com>
2024-03-15 11:35:38 -0300

    feat: adding the account routes to router

--- api/router/routes.go
+++ api/router/routes.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/felipeversiane/go-auth/internal/account/routes"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -8,6 +9,7 @@ import (
 func AppRoutes(router *gin.Engine, database *mongo.Database) *gin.RouterGroup {
 	v1 := router.Group("/v1/api")
 	{
+		routes.AccountRoutes(v1, database)
 	}
 	return v1
 }
